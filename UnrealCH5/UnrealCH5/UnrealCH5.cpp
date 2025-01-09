d3243487a0167809ec7c3f1c78e1127bb487df46
SeongJunKang95 <skyrnt777@gmail.com>
2025-01-09 09:26:09 +0900

    d

--- UnrealCH5/UnrealCH5/UnrealCH5.cpp
+++ UnrealCH5/UnrealCH5/UnrealCH5.cpp
@@ -2,8 +2,8 @@
 
 #include <iostream>
 #include "MyActor.h"
-#include "Math/UnrealMathUtility.h" // 랜덤 함수 사용
-#include "Engine/Engine.h" // UE_LOG 사용
+#include "Math/UnrealMathUtility.h" // 랜덤 함수 사용( FMath::RandRange)
+#include "Engine/Engine.h" // UE_LOG 사용 -> 언리얼 엔진에서 로그를 출력하기 위한 매크로, (LogTep : 로그의 카테고리, Warning : 로그 수준, TEXT : 출력 문자열(좌표를 출력) )
 
 // Sets default values
 AMyActor::AMyActor()
@@ -11,7 +11,7 @@ AMyActor::AMyActor()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	CurrentPosition = FVector2D(0.0f, 0.0f); // 초기좌표 설정
+	CurrentPosition = FVector2D(0.0f, 0.0f); // CurrentPosition 변수 초기화 (시작 위치는 (0,0) )
 
 }
 
@@ -20,7 +20,7 @@ void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	Move(); // 이동함수 호출
+	Move(); // 이동함수 호출 : 10번 이동 및 좌표 출력
 
 }
 
@@ -41,6 +41,8 @@ void AMyActor::Move() // 10번 랜덤 이동 함수
 	{
 		int32 StepX = Step(); // X 방향 이동 값
 		int32 StepY = Step(); // Y 방향 이동 값
+		
+		// CurrentPosition 갱신 : 현재 위치에 Step 값 추가
 		CurrentPosition.X += StepX;
 		CurrentPosition.Y += StepY;
 
--- UnrealCH5/UnrealCH5/UnrealCh5.h
+++ UnrealCH5/UnrealCH5/UnrealCh5.h
@@ -7,6 +7,11 @@
 #include "GameFramework/Actor.h"
 #include "MyActor.generated.h"
 
+// AMyActor 클래스는 게임 내에서 스폰되며, 시작위치에서 랜덤하게 10번 이동한 후, 각 이동 좌표를 출력
+
+
+
+
 UCLASS()
 class HELLO_API AMyActor : public AActor
 {
@@ -26,10 +31,10 @@ public:
 
 private:
 
-	FVector2D CurrentPosition;
+	FVector2D CurrentPosition; // FVector2D타입 변수로, 액터의 현재 위치를 2D좌표 (X, Y) 로 표현 초기값은 (o.of o.of) 로 설정돠며, move 함수에서 이동할 때 마다 갱신
+	
+	int32 Step(); // 한 스텝에서 이동할 거리를 반환하는 함수 (0 또는 1반환), FMath::RandRange(0, 1)를 사용하여 0 또는 1을 반환
 
-	int32 Step();
-
-	void Move();
+	void Move(); // 시작 위치를 출력, 10번 이동하며 좌표를 갱신하고 출력하는 함수, 이동후 위치 출력 
 
 };
